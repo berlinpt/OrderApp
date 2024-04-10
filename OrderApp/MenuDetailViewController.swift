2ca0e5674e12522efbe3f4a337a3f7f67c29d7c3
Berlin <berlinpt@gmail.com>
2024-04-10 13:24:15 -0400

    View and Edit Order Display Order Notify Order Changes Update Order Badge Delete Items from Order

--- OrderApp/MenuDetailViewController.swift
+++ OrderApp/MenuDetailViewController.swift
@@ -37,6 +37,8 @@ class MenuDetailViewController: UIViewController {
             self.addToOrderButton.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
             self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
         }, completion: nil)
+        
+        MenuController.shared.order.menuItems.append(menuItem)
     }
     
     
